53f6b90316bf2572d0f546b2d05a125a1b6e315b
kingcall <kingcall@hotmail.com>
2020-07-19 21:19:37 +0800

    spring-data reactor

---
+++ reactor-r2dbc/src/test/java/com/kingcall/reactorr2dbc/ReactorR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingcall.reactorr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorR2dbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
